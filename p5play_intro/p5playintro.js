9d9975afa1f23761174e8a0060a9c323673f925e
alson-ang <alson.ang@computhink.com.sg>
2026-07-08 08:43:36 +0000

    Jul 8, 2026, 8:43 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -12,7 +12,7 @@ function setup() {
   strokeWeight(10)    // Outline thickness
 
   circle(400, 200, 100)     // x, y, diameter
-  rect(600, 300, 100, 100) // x, y, width, height
+  rect(600, 300, 100, 100)  // x, y, width, height
 
   // End Basic shape testing
 
