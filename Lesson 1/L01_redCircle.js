65406d741ed3dd3fbda76099a20bd558c7dc8f75
alson-ang <alson.ang@computhink.com.sg>
2026-07-08 08:01:49 +0000

    Jul 8, 2026, 8:01 AM

--- Lesson 1/L01_redCircle.js
+++ Lesson 1/L01_redCircle.js
@@ -2,13 +2,12 @@ function setup() {
     createCanvas(600, 400);
     background(220);
 
-    let x = 5;
-    let y = 10;
-    let total = x + y;
-    
+    // let x = 5;
+    // let y = 10;
+    // let total = x + y;
+    // console.log("Sum: " + total)
 }
 
-let total = 0
 function draw() {
     fill("#fc0000");
     ellipse(300,150,50,50);
@@ -16,7 +15,4 @@ function draw() {
     ellipse(300,200,50,50);
     fill("#4efc03");
     ellipse(300,250,50,50);
-
-    total += 1;
-    console.log("Sum: " + total)
 }
