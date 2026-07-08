0fdaedb7614b3e8095af196ec0075174106bb367
alson-ang <alson.ang@computhink.com.sg>
2026-07-08 07:46:12 +0000

    Jul 8, 2026, 7:46 AM

--- Lesson 1/L01_redCircle.js
+++ Lesson 1/L01_redCircle.js
@@ -5,6 +5,9 @@ function setup(){
 
 function draw(){
     fill("#03d7fc");
+    ellipse(300,150,200,200);
+    fill("#03d7fc");
     ellipse(300,200,200,200);
-    
+    fill("#03d7fc");
+    ellipse(300,250,200,200);
 }
