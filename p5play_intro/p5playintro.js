f4f1e7a094aee21bc4ff985f3db7fac96e6d1bd2
alson-ang <alson.ang@computhink.com.sg>
2026-07-08 08:34:37 +0000

    Jul 8, 2026, 8:34 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -3,8 +3,7 @@ function setup() {
   new Canvas(800, 400);
   background(250); //background color
 
-  ball = new Sprite(400, 200, 300, 300);
-
+  // ball = new Sprite(400, 200, 300, 300);
 
   // Basic shape testing
   // write your codes here
