4fa829b60b418404804e78e273465cf477983b4d
alson-ang <alson.ang@computhink.com.sg>
2026-07-08 08:59:00 +0000

    Jul 8, 2026, 8:59 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -20,7 +20,7 @@ function setup() {
    // Create a bouncing ball sprite
    // write your codes here
    ball = new Sprite(400, 200, 100);
-   ball.color = "#"
+   ball.color = "#f5e505"
 }
 
 function draw() {
