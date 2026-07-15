4bb1a903a7827b99dc7c5e25a769ed00f36d9b36
alson-ang <alson.ang@computhink.com.sg>
2026-07-15 07:54:28 +0000

    Jul 15, 2026, 7:54 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -24,7 +24,7 @@ function setup() {
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
   for (let i = 0; i < 10; i++) {
-    
+    console.log(i);
   }
 
   // --- Exercise: Age category classification ---
