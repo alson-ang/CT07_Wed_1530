7a8445ff228aed494bbe6474c22783360afc7381
alson-ang <alson.ang@computhink.com.sg>
2026-07-15 07:41:51 +0000

    Jul 15, 2026, 7:41 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -2,7 +2,7 @@ function setup() {
   // write your codes here
   // Set up the canvas
   new Canvas(800, 400);
-  background(250); //background color
+  background(250);  // Background colour
 
   console.log("Hello.");  // Print
 
