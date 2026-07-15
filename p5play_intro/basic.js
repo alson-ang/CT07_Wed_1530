5eea13d537244c4ffbf8bfc79b3dbcb3b50f3207
alson-ang <alson.ang@computhink.com.sg>
2026-07-15 08:25:36 +0000

    Jul 15, 2026, 8:25 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -59,6 +59,6 @@ function setup() {
 
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
-  let groceries = ["asdasd" , "asdas"]
+  let groceries = ["apple" , "bread", "milk"]
 }
 
