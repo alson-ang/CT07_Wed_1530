33383bfd121ff2f4d96f4bb173047f90299f6516
alson-ang <alson.ang@computhink.com.sg>
2026-07-15 07:48:15 +0000

    Jul 15, 2026, 7:48 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -15,7 +15,9 @@ function setup() {
   // write your codes here
   let base = 10;
   let height = 5;
-  let area = 0.5 
+  let area = 0.5 * base * height;
+  console.log("Area: " + area);
+  
 
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
