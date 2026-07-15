d8b01fa1f81aeb81137f8979bfe8d2718d240e94
alson-ang <alson.ang@computhink.com.sg>
2026-07-15 07:48:04 +0000

    Jul 15, 2026, 7:48 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -13,7 +13,9 @@ function setup() {
 
   // --- Exercise: Area of Triangle ---
   // write your codes here
-
+  let base = 10;
+  let height = 5;
+  let area = 0.5 
 
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
