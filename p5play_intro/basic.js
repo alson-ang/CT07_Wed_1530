8eced49b4a9f21ecc9c338c729ff8088700b54be
alson-ang <alson.ang@computhink.com.sg>
2026-07-15 07:40:05 +0000

    Jul 15, 2026, 7:40 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -4,7 +4,7 @@ function setup() {
   new Canvas(800, 400);
   background(250); //background color
 
-  console.log()
+  console.log("Hello.")
 
   // --- Exercise: Area of Triangle ---
   // write your codes here
