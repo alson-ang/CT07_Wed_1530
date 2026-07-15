b9169b06f61ba9448a3563c6cbf7a8e4556ac333
alson-ang <alson.ang@computhink.com.sg>
2026-07-15 07:47:53 +0000

    Jul 15, 2026, 7:47 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -4,16 +4,16 @@ function setup() {
   new Canvas(800, 400);
   background(250);  // Background colour
 
-  console.log("Hello.");  // Print
+  // console.log("Hello.");  // Print
 
-  let a = 1;
-  let b = 2;
-  let sum = a + b;
-  console.log("Sum: " + sum);
+  // let a = 1;
+  // let b = 2;
+  // let sum = a + b;
+  // console.log("Sum: " + sum);
 
   // --- Exercise: Area of Triangle ---
   // write your codes here
-  
+
 
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
