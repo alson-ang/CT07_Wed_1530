9cd04a2ab5ca405b29012039400804cfd646347f
alson-ang <alson.ang@computhink.com.sg>
2026-07-15 08:33:01 +0000

    Jul 15, 2026, 8:33 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -65,6 +65,6 @@ function setup() {
   groceries.shift();  // Remove first item
   // groceries.pop();  // Remove last item
   // groceries.splice(1, 1); // (num of items to remove, starting index)
-  groceries.splice(1, 1, "kaya"); // (num of items to remove, starting index, )
+  groceries.splice(1, 1, "kaya"); // (num of items to remove, starting index, item to insert)
   console.log(groceries);
 }
