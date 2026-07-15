8ab187ab4ba106ef6a492756f8652c69907a0802
alson-ang <alson.ang@computhink.com.sg>
2026-07-15 08:04:53 +0000

    Jul 15, 2026, 8:04 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -24,6 +24,7 @@ function setup() {
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
   let sum = 0;
+  let yPos = 50
   for (let i = 2; i <= 20; i += 2) {
     console.log(i);
     sum += i;
