cdde63ba1d263d6beace0e36671c72c392c26ee0
alson-ang <alson.ang@computhink.com.sg>
2026-07-22 08:53:51 +0000

    Jul 22, 2026, 8:53 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -19,7 +19,7 @@ function setup() {
     world.gravity.y = 10;
 
     // Debug text
-    fill("#fcba03");   // Text colour
+    fill(252, 186, 3);   // Text colour
 
     // Create bird sprite
     bird = new Sprite();
