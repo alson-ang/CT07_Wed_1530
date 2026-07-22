cfed015dd3a71522440e7c5a8de90af2eb8a4f8e
alson-ang <alson.ang@computhink.com.sg>
2026-07-22 08:48:33 +0000

    Jul 22, 2026, 8:48 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -41,7 +41,7 @@ function setup() {
 function draw() {
     image(background, 0, 0, width, height); // (image, x, y, width, height)
 
-    if (kb.presses("space") || button.mouse.presses("left")) {
+    if (kb.presses("space") || mouse.presses("left")) {
         bird.vel.y = -5;   // Up velocity
     }
 }
