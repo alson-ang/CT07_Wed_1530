c800f1459015674ed7f650b92a995fdd05920bb4
alson-ang <alson.ang@computhink.com.sg>
2026-07-22 08:34:35 +0000

    Jul 22, 2026, 8:34 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -35,7 +35,7 @@ function setup() {
     floor.height = 125;
     floor.x = width / 2;
     floor.y = height - 20;
-    floor.collider = "static";  // Will not move
+    floor.collider = "static";  // Collidable but will not move
 }
 
 function draw() {
