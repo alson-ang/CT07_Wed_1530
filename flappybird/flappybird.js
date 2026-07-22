ba1bbb7ce48acfd7cc3265a26eed88b1bfb7c762
alson-ang <alson.ang@computhink.com.sg>
2026-07-22 08:24:12 +0000

    Jul 22, 2026, 8:24 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -21,7 +21,7 @@ function setup() {
     bird.img = birdMidImg;
     bird.width = 30;
     bird.height = 30;
-    bird.mass = 2;
+    bird.mass = 2;  // How heavy
     bird.drag = 0.02;
     bird.bounciness = 0.5;
     bird.collider = "dynamic";
