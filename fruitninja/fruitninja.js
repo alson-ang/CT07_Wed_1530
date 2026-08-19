d9e01dbc8b30885da04d162f655852445e18f27a
alson-ang <alson.ang@computhink.com.sg>
2026-08-19 08:26:31 +0000

    Aug 19, 2026, 8:26 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -27,6 +27,15 @@ function setup() {
     peach.mass = 2;  // How heavy, heavier = affected by gravity more
     peach.drag = 0.02;   // Air resistance, higher = more resistance
     peach.collider = "dynamic";  // Collidable, movable and affected by physics
+
+    peach = new Sprite();
+    peach.img = peachWhole;
+    peach.width = 500;
+    peach.height = 500;
+    peach.scale = peach.width / peachWhole.width;
+    peach.mass = 2;  // How heavy, heavier = affected by gravity more
+    peach.drag = 0.02;   // Air resistance, higher = more resistance
+    peach.collider = "dynamic";  // Collidable, movable and affected by physics
 }
 
 function draw() {
