7e4384accc8b77c315a08f336875ae3f5c506fd5
alson-ang <alson.ang@computhink.com.sg>
2026-08-19 08:44:24 +0000

    Aug 19, 2026, 8:44 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -36,9 +36,8 @@ function setup() {
 function draw() {
     image(background, 0, 0, width, height); // (image, x, y, width, height)
 
-    
+    if (f)
     spawnPeach();
-    wait(1);
 }
 
 function spawnPeach() {
