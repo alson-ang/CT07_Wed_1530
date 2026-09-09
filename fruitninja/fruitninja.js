dd31dca6dfe68a21e0f3df41a3bfd8a1a567a669
alson-ang <alson.ang@computhink.com.sg>
2026-09-09 09:08:02 +0000

    Sep 9, 2026, 9:08 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -103,8 +103,9 @@ function spawnFruit() {
     let fruitData = random(fruitTypes); // pick one at random
     let randomX = random(300, 500); // random X to spawn. Rem that canvas width is 800
     let fruit = new fruitGroup.Sprite(randomX, height+20, 40); // spawn at bottom
-    
     fruit.image = fruitData.whole; // load image for whole
+    fruit.w = 40;
+    fruit.h = 40;
     fruit.type = fruitData; // store reference to its type i.e. peach or watermelon
     fruit.vel.y = random(-10, -14); // shoot upward at random velocity. Adjust to try!
     fruit.vel.x = random(-2, 2); // sideways curve. Adjust to try!
