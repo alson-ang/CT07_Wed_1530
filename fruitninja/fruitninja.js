487a732009f76f47b02a4501731016bcfa1ec914
alson-ang <alson.ang@computhink.com.sg>
2026-09-09 08:21:58 +0000

    Sep 9, 2026, 8:21 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -110,6 +110,7 @@ function splitFruit(x, y, fruitData) {
     left.life = 60; // remove after 30 frames
     left.collider = "dynamic"; // dynamic - yes gravity and yes collision / static - no gravity and yes collision / none - no gravity and no collision
     left.overlaps(allSprites); // disable collision
+    left.layer = 1;
 
     // create right half
     let right = new fruitHalves.Sprite(x + 10, y, 40, 40);
