f0ceae71a49619f8b93c46c509d837df9de78d6d
alson-ang <alson.ang@computhink.com.sg>
2026-09-09 07:50:01 +0000

    Sep 9, 2026, 7:50 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -1,58 +1,117 @@
 // Variables
 let background;
-let peach;
-let watermelon;
-
-let peachWhole;
-let watermelonWhole;
+let fruitGroup; // group for whole fruits
+let fruitTypes  = []; // store fruit image objects
+let trail;
+let fruitHalves; // new group for sliced halves
 
 function preload() {
     // Load image
     background = loadImage("assets/dojobackground.png");
-    peachWhole = loadImage("assets/peachwhole.png");
-    watermelonWhole = loadImage("assets/watermelonwhole.png");
+    // decalre the peach object
+    let peach = {
+        whole: loadImage("assets/peachwhole.png"),
+        half1: loadImage("assets/peachhalf.png"),
+        half2: loadImage("assets/peachhalf2.png"),
+    }
+
+    // declare the watermelon object
+    let watermelon = {
+        whole: loadImage("assets/watermelonwhole.png"),
+        half1: loadImage("assets/watermelonhalf.png"),
+        half2: loadImage("assets/watermelonhalf.png"),
+    }
+    
+    // store the fruit objects into an array
+    fruitTypes = [peach, watermelon];
+
 }
 
 function setup() {
     // Create canvas
     new Canvas(800, 600);
     world.gravity.y = 10;
-    
-    // Sprites
-    watermelon = new Sprite();
-    watermelon.img = watermelonWhole;
-    watermelon.width = 100;
-    watermelon.height = 100;
-    watermelon.scale = watermelon.width / watermelonWhole.width;
-    watermelon.mass = 2;
-    watermelon.drag = 0.02;
-    watermelon.collider = "dynamic";
-
-    // Group
-    peachGroup = new Group();
+    fruitGroup = new Group();
+  
+    fruitHalves = new Group(); // group for fruit halves
 }
 
 function draw() {
-    image(background, 0, 0, width, height); // (image, x, y, width, height)
+    clear();
+    image(background, 0, 0, width, height); // image(image, x, y, width, height)
 
     // Debug text
-    fill("#ba0000");   // Text colour
+    fill("#dadada") // Text colour
     textSize(50);
-    text("frameCount: " + frameCount, 10, 40);
+    text("frameCount:"+ frameCount, 10, 40);
 
-    if (frameCount % 60 == 0) {
-        spawnPeach();   
+    // call spawnFruit function
+    if (frameCount % 120 == 0) {
+        spawnFruit();
+    }
+
+    // handle slicing when mouse is pressed
+    if (mouse.pressing()){
+        trail = new Sprite(mouse.x, mouse.y, 7);
+        trail.collider = 'none';
+        trail.color = 'red';
+        trail.life = 10;
+        sliceFruit(); // add this line to call function
+    }
+
+   
+}
+// check if any fruit is sliced by the mouse
+function sliceFruit(){
+    for (let fruit of fruitGroup) {
+        if (fruit.sliced){
+            continue; // skip already sliced fruits
+        }
+
+        // calculate distance between mouse and fruit
+        let d = dist(mouse.x, mouse.y, fruit.x, fruit.y);
+
+        if (d < ((fruit.d / 2) + 5)) {
+            fruit.sliced = true; // prevent repeat slicing
+
+            const fx = fruit.x; // x coordinate for the sliced food
+            const fy = fruit.y; // y coordinate for the sliced food
+
+            fruit.remove(); // remove whole fruit
+
+            splitFruit(fx, fy, fruit.type); // spawn halves
+
+            break; // only slice one fruit per frame
+        }
     }
 }
 
-function spawnPeach() {
-    // Create peach sprite in group
-    peach = new peachGroup.Sprite()
-    peach.img = peachWhole;
-    peach.width = 100;
-    peach.height = 100;
-    peach.scale = peach.width / peachWhole.width;
-    peach.mass = 2;  // How heavy, heavier = affected by gravity more
-    peach.drag = 0.02;   // Air resistance, higher = more resistance
-    peach.collider = "dynamic";  // Collidable, movable and affected by physics
-}
+function spawnFruit(){
+        let fruitData = random(fruitTypes); // pick one at random
+        let randomX = random(300, 500); // random X to spawn. Rem that canvas width is 800
+        let fruit = new fruitGroup.Sprite(randomX, height+20, 40); // spawn at bottom
+        fruit.image = fruitData.whole; // load image for whole
+        fruit.type = fruitData; // store reference to its type i.e. peach or watermelon
+        fruit.vel.y = random(-10, -14); // shoot upward at random velocity. Adjust to try!
+        fruit.vel.x = random(-2, 2); // sideways curve. Adjust to try!
+        fruit.friction = 0; // no friction
+    }
+
+    // split a fruit into two halves and animate them
+    function splitFruit(x, y, fruitData) {
+        // create left half
+        let left = new fruitHalves.Sprite(x - 10, y, 40, 40);
+        left.img  = fruitData.half1;
+        left.vel.x = -3; // veer left
+        left.vel.y = random(-5, -2);
+        left.rotationSpeed = -5;
+        left.life = 60; // remove after 30 frames
+
+        // create right half
+        let right = new fruitHalves.Sprite(x + 10, y, 40, 40);
+        right.img = fruitData.half2;
+        right.vel.x = 3; // veer right
+        right.vel.y = random(-5, -2);
+        right.rotationSpeed = 5;
+        right.life = 60; // remove after 30 frames
+        }
