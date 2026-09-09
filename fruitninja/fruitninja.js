ba2b044ce509ff2c68f751095cee74d8ffb145b5
alson-ang <alson.ang@computhink.com.sg>
2026-09-09 08:18:02 +0000

    Sep 9, 2026, 8:18 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -96,7 +96,7 @@ function spawnFruit() {
     fruit.vel.x = random(-2, 2); // sideways curve. Adjust to try!
     fruit.friction = 0; // no friction
     fruit.overlaps(allSprites); // disable collision
-    fruit.layer
+    fruit.layer = 2;
 }
 
 // split a fruit into two halves and animate them
